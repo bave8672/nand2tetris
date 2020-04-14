5a158f136b49a5eeef30ba6aea5d9df5e1bfe126
Ben <avesbenjamin@gmail.com>
2020-04-14 16:38:48 +1000

    fix: assembler labelling bug

--- projects/06/assembler/src/cli/cli.ts
+++ projects/06/assembler/src/cli/cli.ts
@@ -1,23 +1,27 @@
 import * as fs from "fs";
 import * as readline from "readline";
-import { Compiler } from "../compiler/compiler";
 import * as yargs from "yargs";
+import { Compiler } from "../compiler/compiler";
 
-yargs.demandOption("file", "Please provide an asm source file path");
-const file = yargs.argv.file as string;
+const fileIn = (yargs.argv.file || yargs.argv.in) as string;
+if (!fileIn) {
+    throw new Error(`Please provide a .asm file input using --file or --in`);
+}
+const fileOut = (yargs.argv.out || `${fileIn}.out`) as string;
 
 async function main() {
-    const fileStream = fs.createReadStream(file);
+    const fileStream = fs.createReadStream(fileIn);
     const rl = readline.createInterface({
         input: fileStream,
         crlfDelay: Infinity,
     });
-    fs.writeFileSync(`${file}.out`, "");
-    const out = fs.createWriteStream(`${file}.out`, { flags: "a" });
+    fs.writeFileSync(fileOut, "");
+    const out = fs.createWriteStream(fileOut, { flags: "a" });
     const compiler = new Compiler();
     for await (const line of compiler.compile(rl)) {
         out.write(line);
     }
+    console.log(`Built hack file ${fileOut}`);
 }
 
 main().catch(console.error);
